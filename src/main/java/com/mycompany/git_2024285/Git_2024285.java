1ca4d643d86c4984b8a36176060c1d66bdf88dcf
Caiocesars28 <caio650650@gmail.com>
2024-12-30 19:48:01 +0000

    First Project

---
+++ src/main/java/com/mycompany/git_2024285/Git_2024285.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.git_2024285;
+
+/**
+ *
+ * @author caio6
+ */
+public class Git_2024285 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
