beae8c3fa4a0536a66ef7c33f7af980672227d8c
Caiocesars28 <caio650650@gmail.com>
2024-12-30 19:55:34 +0000

    updating code

--- src/main/java/com/mycompany/git_2024285/Git_2024285.java
+++ src/main/java/com/mycompany/git_2024285/Git_2024285.java
@@ -12,5 +12,7 @@ public class Git_2024285 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        
+        System.out.println("Caio - 2024285");
     }
 }
